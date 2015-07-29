72e1a87d5a0edd5daa96a610bcfc5260c44a78fb
baerdaniel <dbaer@Daniel-Bar-2.local>
2015-07-29 11:55:59 +0100

    Images and videos
    Implementing galleries and videos

--- wp-content/themes/TypeList/content-single.php
+++ wp-content/themes/TypeList/content-single.php
@@ -37,50 +37,99 @@
 
 	<div class="entry-content">
 		<?php the_content() ?>
+
+<!-- 		<?php 
+
+				// check for rows (parent)
+				if( have_rows('participants') ): ?>
+					<div class="participants">
+					<?php 
+
+					// loop through rows (parent)
+					while( have_rows('participants') ): the_row(); ?>
+
+						
+						<div>
+
+
+							<h3><?php the_sub_field('name'); ?></h3>
+							<h3><?php the_sub_field('bio'); ?></h3>
+							<?php the_sub_field('image'); ?>
+							
+							
+						</div>	
+
+					<?php endwhile; ?>
+					</div>
+				<?php endif; ?>
+ -->
+
+
 		<?php
 
-			     
-			    // Galleries
-			    while ( have_rows('gallery') ) : the_row();
-
-			        // check if the nested repeater field has rows of data
-		        	if( have_rows('gallery') ):
-
-					 	echo '<ul>';
-
-					 	// loop through the rows of data
-					    while ( have_rows('images') ) : the_row();
-
-							$image = get_sub_field('image');
-
-							echo '<li><img src="' . $image['url'] . '" alt="' . $image['alt'] . '" /></li>';
-
-						endwhile;
-
-						echo '</ul>';
-
-					endif;
-
-			    endwhile;
-
-			    // participants
+			    // various
 			    while ( have_rows('participants') ) : the_row();
 
 					$image = get_sub_field('image');
-					$name = get_sub_field('name');
+					$title = get_sub_field('title');
 					$bio = get_sub_field('bio');
 
 			        if( get_row_layout() == 'participant' ):
 
-
-			        	echo '<div class="portrait"><img src="' . $image['url'] . '" alt="' . $image['alt'] . '" /></div>';
-			        	echo "<h3>" . $name . "</h3>";
-			        	echo "<p>" . $bio . "</p>";
+			        	echo '<div class="participant">';
+			        		echo '<div class="portrait"><img src="' . $image['sizes']['large'] . '"></div>';
+			        		echo '<h3 class="">' . $name . '</h3>';
+			        		echo '<p>' . $bio . '</p>';
+			        	echo '</div>';
 
 			        endif;
 
 			    endwhile;
 
+		?>
+
+
+     	<?php 
+
+		$images = get_field('gallery');
+
+		if( $images ): ?>
+		    <ul class='main-gallery'>
+		        <?php foreach( $images as $image ): ?>
+		            <li>
+		                <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
+		                <?php echo $image['caption']; ?>
+		            </li>
+		        <?php endforeach; ?>
+		    </ul>
+		<?php endif; ?>
+
+		<?php
+
+			    // video embed
+			    while ( have_rows('videos') ) : the_row();
+
+					$video = get_sub_field('video');
+
+			        if( get_row_layout() == 'youtube' ):
+			        	echo '<div class="video">';
+			        		echo '<iframe width="100%" height="auto" src="https://www.youtube.com/embed/' . $video . '?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>';
+			        	echo '</div>';
+			        endif;
+
+			        if( get_row_layout() == 'vimeo' ):
+			        	echo '<div class="video">';
+			        		echo '<iframe width="100%" height="auto" src="https://player.vimeo.com/video/' . $video . '?color=ffffff&title=0&byline=0&portrait=0 frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+			        	echo '</div>';
+			        endif;
+
+			    endwhile;
+
+		?>
+
+
+
+		<?php
 
 			    // various
 			    while ( have_rows('content_boxes') ) : the_row();
@@ -93,14 +142,18 @@
 
 			        	echo '<div class="narrow">';
 			        		echo '<div class="image">' . $image . '</div>';
-			        		echo '<h3 class="brown">' . $title . '</h3>';
+			        		echo '<h3 class="">' . $title . '</h3>';
 			        		echo '<p>' . $body . '</p>';
 			        	echo '</div>';
 
 			        endif;
-			         if( get_row_layout() == 'wide' ):
+			        if( get_row_layout() == 'wide' ):
 
-			        	echo '<div class="wide">' . $title . '</div>';
+			        	echo '<div class="wide">';
+			        		echo '<div class="image">' . $image . '</div>';
+			        		echo '<h3 class="">' . $title . '</h3>';
+			        		echo '<p>' . $body . '</p>';
+			        	echo '</div>';
 
 			        endif;
 
